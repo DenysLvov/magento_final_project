076a63a0b051a5633c946757a3075a4e4134b86e
Den Lvov <lionpdv@gmail.com>
2019-12-04 21:00:04 +0200

    Add parallel exec. Update Check Shopping Cart: - add check prices - update locators

--- src/main/java/denys/DriverManager.java
+++ src/main/java/denys/DriverManager.java
@@ -3,6 +3,8 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,16 +22,27 @@ public class DriverManager {
     }
 
     private static void initDriver() {
-        WebDriverManager.chromedriver().setup();
-        threadDriver.set(new ChromeDriver());
-
-        threadDriver.get().manage().window().fullscreen();
-        threadDriver.get().manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        String browser = System.getProperty("browser", "firefox");
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            //ChromeOptions options = new ChromeOptions();
+            threadDriver.set(new ChromeDriver());
+        }
+        if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            threadDriver.set(new FirefoxDriver());
+        }
+        if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            threadDriver.set(new EdgeDriver());
+        }
+        //threadDriver.get().manage().window().fullscreen();
+        threadDriver.get().manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
         threadDriver.get().manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
     }
 
     public static void killDriver(){
-        threadDriver.get().close();
+        threadDriver.get().quit();
         threadDriver.remove();
     }
 }
